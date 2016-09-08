7034e779ab6541bdbc0f1c81b96d8878f2de8cfe
Artem Russkikh <rusartx@gmail.com>
2016-09-08 18:36:35 +0500

    Spree::FxRate.create_supported_currencies creates currencies

--- spec/models/spree/fx_rate_spec.rb
+++ spec/models/spree/fx_rate_spec.rb
@@ -3,4 +3,14 @@ RSpec.describe Spree::FxRate, type: :model do
 
   it('#value') { expect(subject).to respond_to(:value) }
   it('#currency') { expect(subject).to respond_to(:currency) }
+
+  context '.create_supported_currencies' do
+    subject { described_class }
+
+    before(:each) { Spree::Config.supported_currencies = 'USD, EUR' }
+
+    it 'creates currencies from spree-multi-currency gem' do
+      expect { subject.create_supported_currencies }.to change { subject.count }
+    end
+  end
 end
