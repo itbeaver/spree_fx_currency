a2a213fd1b567cc5e0da60c38491070f6dc186fa
Artem Russkikh <rusartx@gmail.com>
2016-09-09 04:30:40 +0500

    Spec pass

--- app/models/spree/fx_rate.rb
+++ app/models/spree/fx_rate.rb
@@ -10,5 +10,28 @@ module Spree
         find_or_create_by(from_currency: main_currency, to_currency: c.upcase)
       end
     end
+
+    # @todo: implement force option for only applying
+    #        fx rate changes to blank prices
+    def update_all_prices
+      Spree::Product.all.each do |product|
+        from_price = if from_currency.blank?
+                       product.master.price
+                     else
+                       product.price_in(from_currency.upcase)
+                     end
+        raise 'from_price is new_record' if from_price.new_record?
+
+        new_price = product.price_in(to_currency.upcase)
+        new_price.amount = from_price.amount * rate
+        new_price.save if new_price.changed?
+
+        product.variants.each do |variant|
+          new_price = variant.price_in(to_currency.upcase)
+          new_price.amount = from_price.amount * rate
+          new_price.save if new_price.changed?
+        end
+      end
+    end
   end
 end
--- spec/models/spree/fx_rate_spec.rb
+++ spec/models/spree/fx_rate_spec.rb
@@ -22,30 +22,53 @@ RSpec.describe Spree::FxRate, type: :model do
       )
     end
 
-    context 'when prices with currency=to_currency exists' do
-      let!(:from_price) { create(:price, currency: 'USD', amount: 10.00) }
-      let!(:to_price) { create(:price, currency: 'EUR', amount: 5.00) }
+    context 'only master price' do
+      context 'when prices with currency=to_currency exists' do
+        let!(:product) { create(:product, price: 10.00) }
+        let(:variant) { product.master }
+        let(:from_price) { master.price }
+        let!(:to_price) do
+          create(:price, currency: 'EUR', amount: 5.0, variant: variant)
+        end
 
-      subject { to_price.display_amount }
+        it 'updates prices using from_currency price and rate' do
+          expect { fx_rate.update_all_prices }.to change {
+            to_price.reload.display_amount.to_s
+          }.from('€5.00').to('€8.85')
+        end
+      end
 
-      it 'updates prices using from_currency price and rate' do
-        expect { fx_rate.update_all_prices }.to change { subject.to_s }
-          .from('€5.00').to('€8.85')
+      context 'when prices with currency=to_currency not exists' do
+        let!(:product) { create(:product, price: 10.00) }
+        let(:variant) { product.master }
+        let(:from_price) { master.price }
+
+        it 'creates prices using from_currency price and rate' do
+          variant_prices = variant.prices
+
+          expect { fx_rate.update_all_prices }.to change {
+            variant_prices.find_by(currency: fx_rate.to_currency)
+                          .try(:display_amount).to_s
+          }.from('').to('€8.85')
+        end
       end
     end
 
-    context 'when prices with currency=to_currency not exists' do
-      let!(:from_price) { create(:price, currency: 'USD', amount: 10.00) }
+    context 'when product have variants' do
+      context 'when prices with currency=to_currency not exists' do
+        let!(:product) { create(:product, price: 10.00) }
+        let(:master_variant) { product.master }
+        let!(:variant) { create(:variant, product: product, price: 10.00) }
+        let(:from_price) { master.price }
 
-      it 'creates prices using from_currency price and rate' do
-        variant_prices = from_price.variant.prices
-        expect { fx_rate.update_all_prices }.to change { variant_prices.count }
-          .from(0).to(1)
+        it 'creates variants prices using from_currency price and rate' do
+          variant_prices = variant.reload.prices
 
-        expect { fx_rate.update_all_prices }.to change do
-          price = variant_prices.find_by(currency: fx_rate.to_currency)
-          price.try(:display_amount).to_s
-        end.from('').to('€8.85')
+          expect { fx_rate.update_all_prices }.to change {
+            variant_prices.reload.find_by(currency: fx_rate.to_currency)
+                          .try(:display_amount).to_s
+          }.from('').to('€8.85')
+        end
       end
     end
   end
