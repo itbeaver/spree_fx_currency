291ddee640f06f42dbe38976e7d311075c6206f9
Artem Russkikh <rusartx@gmail.com>
2016-09-09 02:17:07 +0500

    Comment wrong functional

--- app/models/spree/price_decorator.rb
+++ app/models/spree/price_decorator.rb
@@ -1,7 +1,7 @@
-Spree::Price.class_eval do
-  def amount
-    return self[:amount] if self[:amount].blank?
-    currency_rate = Spree::FxRate.find_by(currency: currency).try(:rate) || 1
-    self[:amount] * currency_rate
-  end
-end
+# Spree::Price.class_eval do
+#   def amount
+#     return self[:amount] if self[:amount].blank?
+#     currency_rate = Spree::FxRate.find_by(currency: currency).try(:rate) || 1
+#     self[:amount] * currency_rate
+#   end
+# end
