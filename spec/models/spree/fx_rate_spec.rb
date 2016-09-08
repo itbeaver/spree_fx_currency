fd843f9e1c337b74240ad9fde23f021e6424a2ff
Artem Russkikh <rusartx@gmail.com>
2016-09-08 18:32:56 +0500

    Respond to model fields

--- spec/models/spree/fx_rate_spec.rb
+++ spec/models/spree/fx_rate_spec.rb
@@ -1,2 +1,6 @@
 RSpec.describe Spree::FxRate, type: :model do
+  subject { described_class.new }
+
+  it('#value') { expect(subject).to respond_to(:value) }
+  it('#currency') { expect(subject).to respond_to(:currency) }
 end
