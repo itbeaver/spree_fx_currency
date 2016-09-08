d194c8a21d28b92c09d299c73ff34c8f96278fdf
Artem Russkikh <rusartx@gmail.com>
2016-09-09 02:56:42 +0500

    Add to_currency and from_currency

--- app/models/spree/fx_rate.rb
+++ app/models/spree/fx_rate.rb
@@ -1,10 +1,14 @@
 module Spree
   class FxRate < Spree::Base
     def self.create_supported_currencies
+      return unless table_exists?
+      main_currency = Spree::Config.currency
       supported_currencies = Spree::Config.supported_currencies
       return if supported_currencies.blank?
       currencies = supported_currencies.split(', ')
-      currencies.each { |c| find_or_create_by(currency: c.upcase) }
+      currencies.each do |c|
+        find_or_create_by(from_currency: main_currency, to_currency: c.upcase)
+      end
     end
   end
 end
--- db/migrate/20160908125149_create_spree_fx_rates.rb
+++ db/migrate/20160908125149_create_spree_fx_rates.rb
@@ -1,7 +1,8 @@
 class CreateSpreeFxRates < ActiveRecord::Migration
   def change
     create_table :spree_fx_rates do |t|
-      t.string :currency
+      t.string :from_currency
+      t.string :to_currency
       t.float :rate, default: 1.00
 
       t.timestamps null: false
