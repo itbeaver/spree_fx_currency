186235987e09d9a945f1e548909c5606bd7e3ed5
Artem Russkikh <rusartx@gmail.com>
2016-09-08 20:26:04 +0500

    Spec pass

--- app/models/spree/price_decorator.rb
+++ app/models/spree/price_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Price.class_eval do
   def amount
+    return self[:amount] if self[:amount].blank?
     currency_rate = Spree::FxRate.find_by(currency: currency).try(:rate) || 1
     self[:amount] * currency_rate
   end
