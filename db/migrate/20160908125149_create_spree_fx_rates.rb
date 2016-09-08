3eea70243570832c57ecf91c3a73e0d4113a29ab
Artem Russkikh <rusartx@gmail.com>
2016-09-08 19:43:32 +0500

    Use float for exchange rate

--- db/migrate/20160908125149_create_spree_fx_rates.rb
+++ db/migrate/20160908125149_create_spree_fx_rates.rb
@@ -2,7 +2,7 @@ class CreateSpreeFxRates < ActiveRecord::Migration
   def change
     create_table :spree_fx_rates do |t|
       t.string :currency
-      t.decimal :value, precision: 10, scale: 2, default: 1.00
+      t.float :rate, default: 1.00
 
       t.timestamps null: false
     end
