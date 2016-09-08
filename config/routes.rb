16227ad6f32fcbfb1b05b355f77411dd52c85016
Artem Russkikh <rusartx@gmail.com>
2016-09-09 02:15:10 +0500

    Improve UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :fx_rates do
       collection do
-        post 'create_supported_currencies'
+        get :create_supported_currencies
       end
     end
   end
