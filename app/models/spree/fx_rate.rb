6024263bb992c7cc6aae1f0adafce95b26f77e5c
Artem Russkikh <rusartx@gmail.com>
2016-09-08 18:52:28 +0500

    Spec pass

--- app/models/spree/fx_rate.rb
+++ app/models/spree/fx_rate.rb
@@ -1,4 +1,8 @@
 module Spree
   class FxRate < Spree::Base
+    def self.create_supported_currencies
+      currencies = Spree::Config.supported_currencies.split(', ')
+      currencies.each { |c| find_or_create_by(currency: c.upcase) }
+    end
   end
 end
