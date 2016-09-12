290a5041400fcf960b034fafae37a30dd9bb165c
Artem Russkikh <rusartx@gmail.com>
2016-09-12 21:34:23 +0500

    Require pry and webmock in spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,9 +15,11 @@ ENV['RAILS_ENV'] = 'test'
 
 require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
+require 'pry'
 require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
